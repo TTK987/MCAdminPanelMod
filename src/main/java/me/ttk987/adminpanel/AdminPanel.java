b7a09592c3b04c076904911f0c6045772ca605cc
TTK987 <99139882+TTK987@users.noreply.github.com>
2023-04-09 21:13:09 +0200

    Add files via upload

---
+++ src/main/java/me/ttk987/adminpanel/AdminPanel.java
@@ -0,0 +1,13 @@
+package me.ttk987.adminpanel;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AdminPanel implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
